494a5a451ed38dd7ede45b413d9661d1f1b88070
deadled <51926166+DeadleD@users.noreply.github.com>
2019-07-05 18:12:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+ task - 1 - https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR37Wnlnp3RIM5X5EDt0qkdqM2GP6xV0mCh_DI0LfFe68Cefcsx7vtloT4o
+ 
+function circleCircumference(circle) {
+  return (circle.radius * 2) * Math.PI;
+}
+
+task - 2 - https://www.codewars.com/kata/training-js-number-13-number-object-and-its-properties/train/javascript
